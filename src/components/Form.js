9731287b0afd413f45e05850790732891a791341
FelProg <felixgerman@hotmail.com>
2023-06-13 16:53:31 -0700

    handling 2 field form input with 1 useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,22 +2,36 @@ import React from "react";
 
 export default function Form(){
 
-    const [inputData,setInputData] = React.useState("");
+    const [inputData,setInputData] = React.useState({firstName:"",lastName:""});
 
     console.log(inputData);
 
     function handleInput(event){
-        setInputData(event.target.value)
+        setInputData( prevValue => {
+            return{
+                ...prevValue, [event.target.name]:event.target.value
+            }
+        })
     }
 
 
     return(
         <form>
-        <input
-        type="text"
-        onChange={handleInput}
-        >
-        </input>
+            <label htmlFor="firstName">First name</label>
+            <input
+                name="firstName"
+                type="text"
+                onChange={handleInput}
+            >
+            </input>
+            <br />
+            <label htmlFor="lastName">Last name</label>
+            <input
+                name="lastName"
+                type="text"
+                onChange={handleInput}
+            >
+            </input>
         </form>
     );
 }
